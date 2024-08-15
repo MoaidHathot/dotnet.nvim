8e258b814fe212e4456dd0e1d230aea3b8f57f36
Moaid Hathot <MoaidHathot@users.noreply.github.com>
2024-08-15 12:15:23 -0700

    Update path-utils.lua
    Revert whitespaces and formatting and using regex instead of two "contains"

--- lua/dotnet/utils/path-utils.lua
+++ lua/dotnet/utils/path-utils.lua
@@ -1,12 +1,8 @@
 local log = require("plenary.log"):new()
-
 local M = {}
-
 local scan = require "plenary.scandir"
-
 function M.tbl_contains_pattern(t, pattern)
   vim.validate { t = { t, "t" } }
-
   for _, v in ipairs(t) do
     if string.match(v, pattern) then
       return true
@@ -14,15 +10,11 @@ function M.tbl_contains_pattern(t, pattern)
   end
   return false
 end
-
 function M.cwd_contains_pattern(pattern)
   local dir = scan.scan_dir(".", { hidden = true, depth = 1 })
-
   local result = M.tbl_contains_pattern(dir, pattern)
-
   return result
 end
-
 function M.is_directory(path)
   if vim.fn.isdirectory(path) == 1 then
     return true
@@ -30,89 +22,76 @@ function M.is_directory(path)
     return false
   end
 end
-
 function M.get_last_path_part(path)
 	local part = nil
 	for current_match in string.gmatch(path, "[^/\\]+") do
 		part = current_match
 	end
-
 	return part
 end
-
 function M.get_parent_directory(path)
   local directory
-
   local split = path:match "/[^/]*$"
-
   if split ~= nil then
     directory = path:gsub(split, "")
   end
-
   return directory
 end
 
 function M.get_file_path_namespace(file_path)
-	local path = file_path
-	local path_tmp
-	local cwd = vim.fn.getcwd()
-	local project
-	local project_parent
-	local dir
+  local path = file_path
+  local path_tmp
+  local cwd = vim.fn.getcwd()
+  local project
+  local project_parent
+  local dir
 
-	path = M.get_parent_directory(path)
-	path_tmp = path
+  path = M.get_parent_directory(path)
+  path_tmp = path
 
-	-- Get project path
-	if file_path ~= nil then
-		while true do
-			dir = scan.scan_dir(path, { hidden = true, depth = 1 })
+  -- Get project path
+  if file_path ~= nil then
+    while true do
+      dir = scan.scan_dir(path, { hidden = true, depth = 1 })
 
-			if M.tbl_contains_pattern(dir, ".*.csproj") or M.tbl_contains_pattern(dir, ".*.fsproj") then
-				project = path
-				log.warn("Breaking ...")
-				break
-			end
+      if M.tbl_contains_pattern(dir, ".*%.[fc]sproj") then
+        project = path
+        break
+      end
 
-			if path == cwd then
-				return cwd
-			end
+      if path == cwd then
+        return cwd
+      end
 
-			path = M.get_parent_directory(path)
-		end
+      path = M.get_parent_directory(path)
+    end
 
-		if project and path_tmp then
-			project_parent = M.get_parent_directory(project)
+    if project and path_tmp then
+      project_parent = M.get_parent_directory(project)
 
-			return path_tmp:gsub(project_parent, ""):sub(2):gsub("/", ".")
-		else
-			log.error("Failed to find parent project")
-			return cwd
-		end
-	else
-		log.error("Failed to find parent project")
-		return cwd
-	end
+      return path_tmp:gsub(project_parent, ""):sub(2):gsub("/", ".")
+    else
+      log.error("Failed to find parent project")
+      return cwd
+    end
+  else
+    log.error("Failed to find parent project")
+    return cwd
+  end
 end
 
 function M.get_projects()
-	local csproj_paths = scan.scan_dir(vim.fn.getcwd(), { search_pattern = ".*.csproj$" }) or {}
-	local fsproj_paths = scan.scan_dir(vim.fn.getcwd(), { search_pattern = ".*.fsproj$" }) or {}
-	local proj_paths = vim.tbl_extend("force", csproj_paths, fsproj_paths)
-	return proj_paths
+  local project_paths = scan.scan_dir(vim.fn.getcwd(), { search_pattern = ".*%.[fc]sproj$" })
+  return project_paths
 end
-M.get_projects()
 
 M.get_project_name_and_directory = function(name_with_path)
 	name_with_path = string.gsub(name_with_path, "\\", "/")
 	local directory = string.match(name_with_path, "(.+/)[^/\\]+")
-
 	if directory == nil or directory == '' or directory == './' then
 		directory = ''
 	end
-
 	local name = string.gsub(name_with_path, directory, '')
-
 	return {
 		project_name = name,
 		project_directory = directory,
